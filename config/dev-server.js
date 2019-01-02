58173cebb4890de89e8ec7c44638ba0ed082bfc5
jianchao.wang <1023024593@qq.com>
2019-01-02 18:41:24 +0800

    feat: config包结构优化

---
+++ config/dev-server.js
@@ -0,0 +1,16 @@
+
+const context = ["/wjc"];
+
+const pathRewrite = {'/wjc': ''};
+
+context.map((item) => {
+	pathRewrite[`^${item}`] = '';
+})
+module.exports = {
+    port: 9001,
+    porxy: [{
+        context,
+        target: "http://localhost:9999/",
+        pathRewrite
+    }]
+};
